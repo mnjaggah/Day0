7384157f7386b41ee85b43342a4a59df608a06ed
Maria <maria.njaggah@gmail.com>
2017-02-07 04:26:54 +0300

    added comments to my code

--- prime_num.py
+++ prime_num.py
@@ -1,11 +1,14 @@
+#this function determines whether a number is prime or not
+#one can run a list comprehension to get the list of prime numbers
 def is_prime(x):
+    #define the unique conditions i.e 2 is prime
     if x < 2:
         return False
     elif x == 2:
         return True
     else:
         while x > 2:
-            for n in range(2, x):
+            for n in range(2, x): #for loop to evaluate as prime
                 if x % n == 0:
                     return False
                     break
